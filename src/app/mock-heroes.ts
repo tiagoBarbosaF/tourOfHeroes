cf22edec0454ef1b1384c33d99a0951d77faad51
Tiago Barbosa <tiago@farias.cc>
2021-10-16 22:30:46 -0300

    First commit - Tour of Heroes Angular tutorial studies

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Legolas' },
+  { id: 12, name: 'Araton' },
+  { id: 13, name: 'Dr Drue' },
+  { id: 14, name: 'May' },
+  { id: 15, name: 'Ayla' },
+  { id: 16, name: 'Drumpete' },
+  { id: 17, name: 'Tristan' },
+  { id: 18, name: 'Thyrael' },
+  { id: 19, name: 'Gondor' },
+  { id: 20, name: 'Arthuria' },
+];
